9638ecb108446b3a9af4be0473b577d7d0e1290c
ManeeshaChandel <maneeshachandel27@gmail.com>
2023-05-25 21:38:34 +0530

    Initial commit

---
+++ src/main/java/com/example/stringcalculatortddkata/StringCalculatorTddKataApplication.java
@@ -0,0 +1,13 @@
+package com.example.stringcalculatortddkata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringCalculatorTddKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringCalculatorTddKataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/stringcalculatortddkata/StringCalculatorTddKataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stringcalculatortddkata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringCalculatorTddKataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
